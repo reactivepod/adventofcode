e601c6eb88fe5941f3356b6441a8e7c2c40f0d49
Fredrik Andersson <fredrik.andersson@widespace.com>
2015-12-04 09:15:17 +0100

    Day 4 solution added

---
+++ 4/fanderzon/index.js
@@ -0,0 +1,23 @@
+const md5 = require('md5');
+const input = 'yzbqklnj',
+      pattern5 = /^00000/,
+      pattern6 = /^000000/;
+
+let counter = 1,
+		hash = '',
+    five,
+    six;
+
+
+while (!pattern5.test(hash)) {
+  hash = md5(input + counter);
+  counter++;
+}
+
+console.log('five zeroes', counter - 1);
+
+while (!pattern6.test(hash)) {
+  hash = md5(input + counter);
+  counter++;
+}
+console.log('six zeroes', counter - 1);
