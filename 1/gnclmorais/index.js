6057e0850a46de6bb626eac113c2d876f73b2a7c
Gonçalo Morais <gnclmorais@gmail.com>
2015-12-04 19:22:04 +0000

    Solve the second part.

--- 1/gnclmorais/index.js
+++ 1/gnclmorais/index.js
@@ -1,12 +1,20 @@
 const fs = require('fs')
 const fl = fs.readFileSync('./input.txt').toString('utf8')
 
-function upOrDown(char) {
-  return char === '(' ? 1 : -1
+function upOrDown(prev, char) {
+  return prev + (char === '(') * 2 - 1
+}
+
+function basementChar(prev, char, index) {
+  if (typeof prev === 'string') return prev
+
+  var floor = upOrDown(prev, char)
+  return floor === -1 ? (index + 1).toString() : floor
 }
 
 function main() {
-  console.log(fl.split('').reduce(upOrDown));
+  console.log('Part 1:', fl.split('').reduce(upOrDown, 0))
+  console.log('Part 2:', fl.split('').reduce(basementChar, 0))
 }
 
-main();
+main()
