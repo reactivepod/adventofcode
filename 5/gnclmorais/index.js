b019823ba3df6a41ddd41e0fec7e447e120c6000
Gonçalo Morais <gnclmorais@gmail.com>
2015-12-06 11:52:35 +0000

    Part 2 of day 5.

--- 5/gnclmorais/index.js
+++ 5/gnclmorais/index.js
@@ -24,12 +24,36 @@ function isNiceString(str) {
   return hasThreeVowels(str) && hasOneLetterTwice(str) && noSpecialStrings(str)
 }
 
+function hasPairNotOverlapping(str) {
+  for (var i = 0, s = str.length - 1; i < s; i += 1) {
+    var pair = str.substr(i, 2)
+    if (str.indexOf(pair) + 1 < str.lastIndexOf(pair)) return true
+  }
+  return false
+}
+
+function hasOneRepeatWithOneBetween(str) {
+  for (var i = 0, s = str.length - 2; i < s; i += 1) {
+    if (str[i] === str[i + 2]) return true
+  }
+  return false
+}
+
+function isNiceStringForPart2(str) {
+  return hasPairNotOverlapping(str) && hasOneRepeatWithOneBetween(str)
+}
+
 function part1() {
   return str.split('\n').filter(isNiceString).length
 }
 
+function part2() {
+  return str.split('\n').filter(isNiceStringForPart2).length
+}
+
 function main() {
   console.log('Part 1:', part1())
+  console.log('Part 2:', part2())
 }
 
 main()
